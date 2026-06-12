890e8ca9087eef62663501b770368eb1addce804
Tadeas Bronis <tadeas.bronis@gmail.com>
2026-06-12 14:05:19 +0200

    Show line numbers in neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -6,6 +6,20 @@ return {
     lazy = false, -- neo-tree will lazily load itself
     config = function()
         require("neo-tree").setup({
+            -- Show absolute + relative line numbers in the tree window so that
+            -- <count>j / <count>k / <count>G work the same as in code buffers.
+            -- Has to be done via event_handlers because Neo-tree windows have
+            -- their own buffer type and a plain FileType autocmd doesn't catch
+            -- every redraw.
+            event_handlers = {
+                {
+                    event = "neo_tree_buffer_enter",
+                    handler = function()
+                        vim.opt_local.number = true
+                        vim.opt_local.relativenumber = true
+                    end,
+                },
+            },
             filesystem = {
                 filtered_items = {
                     visible = true,
@@ -14,6 +28,7 @@ return {
                 },
             },
         })
+
         vim.keymap.set("n", "<leader>t", ":Neotree toggle<CR>", {})
         vim.keymap.set("n", "<leader>e", ":Neotree reveal<CR>", {})
     end
